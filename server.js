4bf38d546ce2acd3dd3dcd5481dd5b1ec9844e9b
oooook916-oss <oooook916@gmail.com>
2026-03-28 21:33:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const WebSocket = require('ws');
+const WebSocket = require('https://atl-clan1.onrender.com');
 const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ port: PORT });
